18d4c3893cfe52c949df850c6338aa8a380daf58
dpatro <patro.dipankar@gmail.com>
2014-03-17 16:09:29 +0530

    Success!

--- api/controllers/GoogleSearchController.js
+++ api/controllers/GoogleSearchController.js
@@ -1,34 +1,23 @@
-var https = require('https');
+var request = require('request');
 
 
 module.exports = {
 
   blogs: function (req, res) {
-  	var apiReqObject = {
-		host : 'ajax.googleapis.com',
-		port : 443,
-		path : '/ajax/services/search/blogs?v=1.0&q='+req.query.q, // the rest of the url with parameters if needed
-		method : 'GET' // do GET
-	}
-	console.log(apiReqObject);
-
-	// do the GET request
-	var reqGet = https.request(apiReqObject, function(apiRes) {
-		// console.log("statusCode: ", apiRes.statusCode);
-		// console.log("headers: ", apiRes.headers);
-		
-		apiRes.on('data', function(d) {
-		    console.log(d);
-		});
-		res.send(apiRes.headers);
-	});
-
-	reqGet.end();
-	
-	reqGet.on('error', function(e) {
-		console.error(e);
-	});
-
+  	console.log(req.query.q);
+  	request.get(
+  		'https://ajax.googleapis.com/ajax/services/search/blogs?v=1.0&q='+req.query.q,
+  		function (error, response, body) {
+	  		if (!error && response.statusCode == 200) {
+	    		res.send(body); // Print the google web page.
+	  		}
+	  		else {
+	  			console.log(error);
+	  			console.log(response);
+	  			res.error(500);
+	  		}
+		}
+	);
   },
 
 
